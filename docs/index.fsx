975bb63d7e8325476b61b031b2d32b52e9d2c31d
Kevin Schneider <schneider.kev@outlook.de>
2021-07-16 14:19:24 +0200

    Set up documentation with fsdocs

---
+++ docs/index.fsx
@@ -0,0 +1,18 @@
+(**
+# TMEA - **T**hermodynamically **M**otivated **E**nrichment **A**nalysis
+[![](https://img.shields.io/nuget/v/TMEA)](https://www.nuget.org/packages/TMEA)
+[![](https://img.shields.io/badge/DOI-10.3390%2Fe22091030-brightgreen)](https://doi.org/10.3390/e22091030)
+
+This repository is home of the framework TMEA ( **T**hermodynamically **M**otivated **E**nrichment **A**nalysis ), which we created from the scripts we used in our 2020 [Entropy paper]( https://doi.org/10.3390/e22091030)
+
+Find the authors on github: [Kevin Schneider](https://github.com/kMutagene) (1), [Benedikt Venn](https://github.com/bvenn) (1), [Timo Mühlhaus](https://github.com/muehlhaus)
+
+- (1) : These authors contributed **equally**.
+
+If you use this package in your research, please cite it. Citation formats are available at the [original article page](https://www.mdpi.com/1099-4300/22/9/1030#cite)
+
+alternatively, here is an example citation:
+
+> Schneider K, Venn B, Mühlhaus T. TMEA: A Thermodynamically Motivated Framework for Functional Characterization of Biological Responses to System Acclimation. Entropy. 2020; 22(9):1030.
+*)
+
